62e4d4866d8ff683306acd77821a5dd192bc0d1e
KosumovicDenis <deniskosumi21@gmail.com>
2026-03-04 23:07:08 +0100

    fix(agent): exclude current waypoint from potential next destinations

--- HRISim_docker/src/HRISim/hrisim_util/src/hrisim_util/Agent.py
+++ HRISim_docker/src/HRISim/hrisim_util/src/hrisim_util/Agent.py
@@ -32,8 +32,7 @@ class Agent:
         self.taskDuration = None
         self.nConsecutiveStuck = 0
         self.closestWPidx = 0
-          
-               
+
     @property
     def closestWP(self):
         if self.x is None or self.y is None:
@@ -43,7 +42,7 @@ class Agent:
 
         # Calculate distances from the agent's position to all waypoints
         distances = [(wp, ((self.x - pos[wp][0]) ** 2 + (self.y - pos[wp][1]) ** 2) ** 0.5) for wp in self.G.nodes]
-        
+
         # Sort by distance
         sorted_distances = sorted(distances, key=lambda x: x[1])
 
@@ -51,7 +50,7 @@ class Agent:
         if not self.isStuck:
             return sorted_distances[0][0]
 
-        # If the agent is stuck, attempt to use the second closest waypoint
+            # If the agent is stuck, attempt to use the second closest waypoint
         else:
             if self.nConsecutiveStuck <= CONSECUTIVE_STUCK_THRESHOLD:
                 return sorted_distances[0][0]
@@ -62,32 +61,31 @@ class Agent:
 
         # Fallback in case there’s only one waypoint
         return sorted_distances[0][0]
-  
+
     @property
     def isFree(self):
         return len(self.path) == 0
-        
+
     @property
     def nextWP(self):
         destname = self.path.pop(0)
         pos = nx.get_node_attributes(self.G, 'pos')
-        
+
         dest = Point()
         dest.x = pos[destname][0]
         dest.y = pos[destname][1]
-        
+
         self.pastDest = self.original_path[max(0, self.original_path.index(destname) - 1)]
         self.currDest = self.original_path[self.original_path.index(destname)]
         self.nextDest = self.original_path[min(len(self.original_path) - 1, self.original_path.index(destname) + 1)]
         self.nextDestPos = [dest.x, dest.y, 0]
-        
+
         return destname, dest
-    
+
     @property
     def finalDest(self):
         return self.original_path[-1] if self.original_path else None
-    
-       
+
     def to_dict(self):
         """Serialize the Agent object to a dictionary, replacing None values with defaults."""
         return {
@@ -131,25 +129,25 @@ class Agent:
         agent.isStuck = data['isStuck']
         agent.taskDuration = data['taskDuration'] if data['taskDuration'] != DEFAULT_VALUE else None
         return agent
-    
+
     def heuristic(self, a, b):
         pos = nx.get_node_attributes(self.G, 'pos')
         (x1, y1) = pos[a]
         (x2, y2) = pos[b]
         return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-    
+
     def selectDestination(self, selected_time, potential_dests):
         destinations = self.schedule[selected_time]['dests']
-        #! I am commenting this line to allow the agents to select the same destination
-        # if self.pastFinalDest is not None and self.pastFinalDest != 'delivery_point': potential_dests.remove(self.pastFinalDest)
-        
+        if self.pastFinalDest is not None:
+            potential_dests.remove(self.pastFinalDest)
+
         # Generate probabilities
         tmp_dest = []
         probabilities = []
         for dest in potential_dests:
             mean = destinations[dest]['mean']
             std = destinations[dest]['std']
-            
+
             if mean == 0: continue
             # probability = stats.norm(mean, std).pdf(mean)
             probability = mean
@@ -160,26 +158,24 @@ class Agent:
         probabilities = np.array(probabilities)
         # Randomly select a destination
         selected_destination = np.random.choice(tmp_dest, p=probabilities)
-        
+
         return selected_destination
-    
-    
-    def setTask(self, destination, duration = None, isStuck = False):
-        if isStuck: 
+
+    def setTask(self, destination, duration=None, isStuck=False):
+        if isStuck:
             self.nConsecutiveStuck += 1
-        else: 
+        else:
             self.nConsecutiveStuck = 0
             self.closestWPidx = 0
-        
+
         self.pastFinalDest = self.finalDest if self.finalDest else None
         self.path = nx.astar_path(self.G, self.closestWP, destination, heuristic=self.heuristic, weight='weight')
         self.original_path = copy.deepcopy(self.path)
-            
+
         self.taskDuration = {wp: 0 for wp in self.path}
         if duration is not None:
             self.taskDuration[self.path[-1]] = duration
-        
-    
+
     def getTaskDuration(self):
         if self.allowTask:
             if self.finalDest.startswith("toilet"):
